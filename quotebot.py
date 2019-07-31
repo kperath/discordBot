4d50504c8d435df494750ef6d73539b46f4769b0
Kunju Perath <kperath@gmail.com>
2019-07-31 16:52:33 -0400

    Added a ton more features for the bot

--- quotebot.py
+++ quotebot.py
@@ -5,7 +5,7 @@ import os
 
 client = discord.Client()
 
-@client.event # event decorator/wrapper
+@client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
@@ -14,8 +14,50 @@ async def on_ready():
 async def on_message(message):
     print(f"{message.author.name} ({message.author}) says \"{message.content}\" in channel #{message.channel}")
 
-    if "fly me to the moon" in message.content.lower():
+    guild = client.get_guild(605965627097743402)
+
+    if "!membercount" in message.content.lower() or "!count" in message.content.lower():
+        await message.channel.send(f"```{guild.member_count}```") # find a way to subtract number of bots!!!!!!
+
+    if "!fly me to the moon" in message.content.lower():
         await message.channel.send("Enjoy :) https://www.youtube.com/watch?v=kTxRm9HD7lo")
+
+    if "!logout" in message.content.lower() or "!leave" in message.content.lower() or "!bye" in message.content.lower():
+        await message.channel.send("Goodbye :wave:")
+        await client.close()
+
+    if "!memberlist" in message.content.lower():
+        
+        for m in guild.members:
+            await message.channel.send(f"```{m.display_name}::{m.top_role} ({m.status})```")
+
+    if "!banlist" in message.content.lower():
+
+        for m in await guild.bans():
+            await message.channel.send(f"```{m.user} {m.reason}```")
+
+    if "!statusreport" in message.content.lower() or "!statuses" in message.content.lower():
+        offline = 0
+        online = 0
+        idle = 0
+        dnd = 0
+
+        for m in guild.members:
+            
+            if str(m.status) == "offline":
+                offline += 1
+
+            elif str(m.status) == "online":
+                online += 1
+
+            elif str(m.status) == "idle":
+                idle += 1
+
+            elif str(m.status) == "dnd":
+                dnd += 1
+
+        await message.channel.send(f"Offline: {offline}\nOnline: {online}\nIdle: {idle}\nDnd: {dnd}")
+
 
 client.run(os.environ.get("BOT_TOKEN"))
 
